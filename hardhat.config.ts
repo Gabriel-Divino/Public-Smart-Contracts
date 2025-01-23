7481f3c25b8e692cb422128bd6d0dad3138ebb1f
Gabriel-Divino <111096168+Hercleiker@users.noreply.github.com>
2025-01-23 11:09:26 -0300

    Configurando redes de deploy

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,38 @@
 import { HardhatUserConfig } from "hardhat/config";
+import dotenv from "dotenv";
+dotenv.config();
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
+  networks:{
+    local:{
+      url : "http://127.0.0.1:8545/",
+      chainId:31337,
+      accounts:{
+        mnemonic : "test test test test test test test test test test test junk"
+      }
+    },
+    ganache : {
+      url : "http://127.0.0.1:7545",
+      chainId : 1337,
+      accounts:{
+        mnemonic : process.env.MNEMONIC_GANACHE
+      }
+    },
+    sepolia:{
+      url:process.env.SEPOLIA_URL,
+      chainId:11155111,
+      accounts:{
+        mnemonic:process.env.MNEMONIC
+      }
+    }
+    
+  },
+  etherscan:{
+    apiKey:process.env.API_KEY
+  }
+
 };
 
 export default config;
